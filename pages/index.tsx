e03f88e750ee8e53085e3a1500c6577fb3695bad
subashstanz <59300356+subashstanz@users.noreply.github.com>
2023-04-15 12:33:04 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
 import CommentBox from './commentBox'
 
 export default function Home() {
